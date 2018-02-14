753c2fc8688374ef8e813b9e4e10d70e7d22d292
abidepp <abinavdepp@gmail.com>
2018-02-14 18:15:41 +0530

    initial project commit

---
+++ app/src/main/java/com/example/admin/practice_one/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.admin.practice_one;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ServiceConnection serviceConnection;
+    MyService myService;
+    Intent serviceIntent;
+    boolean toggle = false;
+    Button bindService, unBindService,getYourMessage, startService;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindService = (Button) findViewById(R.id.bind);
+        unBindService = (Button) findViewById(R.id.unbind);
+        getYourMessage = (Button) findViewById(R.id.message);
+        textView = (TextView) findViewById(R.id.textView2);
+        startService = (Button) findViewById(R.id.startservice);
+
+
+        bindService.setOnClickListener(this);
+        unBindService.setOnClickListener(this);
+        getYourMessage.setOnClickListener(this);
+        startService.setOnClickListener(this);
+
+
+        //service connection API
+        //Intents
+        //bound service toggle
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        int item = v.getId();
+
+        if (item == R.id.bind) {
+
+            Log.i("on click", "on bind clicked");
+
+            if (serviceConnection == null) {
+                Toast.makeText(this, "start the service first", Toast.LENGTH_LONG).show();
+
+            }
+
+            serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                toggle = true;
+                Log.i("on click", "onServiceConnected");
+                MyService.MyServiceBinder myServiceBinder = (MyService.MyServiceBinder) service;
+                myService = myServiceBinder.getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                toggle = false;
+                Log.i("on click", "onServiceDisconnected");
+            }
+            };
+            bindService(serviceIntent, serviceConnection, BIND_AUTO_CREATE);  //this is a bound service--local binding
+
+    }
+
+        if(item == R.id.unbind)
+        {
+            Log.i("on click","on unbind clicked");
+            serviceIntent= new Intent(getApplicationContext(), MyService.class);
+            stopService(serviceIntent);
+        }
+        if(item == R.id.message)
+        {
+            if(toggle)
+            {
+                Log.i("on click","get message clicked");
+                textView.setText(myService.getYourMessage());
+            }
+            else
+            {
+                Toast.makeText(this, "start the service first", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        if(item == R.id.startservice)
+        {
+            serviceIntent= new Intent(getApplicationContext(), MyService.class);
+            startService(serviceIntent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/admin/practice_one/MyService.java
@@ -0,0 +1,83 @@
+package com.example.admin.practice_one;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by admin on 12/02/18.
+ */
+
+public class MyService extends Service {
+
+
+    String yourMessage;
+
+    class MyServiceBinder extends Binder
+    {
+        public MyService getService()
+        {
+            return MyService.this;
+        }
+    }
+
+    private IBinder binder = new MyServiceBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("new bind requested","inside onBind");
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i("new service started","inside onStartCommand");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                generateSweetMessage();
+            }
+        }).start();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy","stop service called");
+        yourMessage = "";
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("onCreate","onCreate called");
+    }
+
+    public void generateSweetMessage()
+    {
+        try {
+            Thread.sleep(1000);
+            String messages = "you are the best BUNNY! Always Excited :P";
+
+                yourMessage = messages;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getYourMessage()
+    {
+        return yourMessage;
+    }
+
+}
